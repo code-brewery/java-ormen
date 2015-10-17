dd1cdd8c9677c721125d4040181095ef220c68ec
Peter Nerg <peter.nerg@gmail.com>
2015-10-17 09:08:35 +0200

    added type inference to Model methods to avoid casting

--- src/main/java/org/codebrewery/JSONConverter.java
+++ src/main/java/org/codebrewery/JSONConverter.java
@@ -1,9 +1,9 @@
 package org.codebrewery;
 
-import org.codehaus.jackson.map.ObjectMapper;
-
 import java.io.IOException;
 import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  * Created by jepp3 on 2015-09-06.
@@ -18,35 +18,34 @@ public class JSONConverter {
     /**
      * Converts a AbstractRestModel to a json string. It will look at the anotations on the elements.
      *
-     * @param restModel the model that should be marshalled
+     * @param restModel
+     *            the model that should be marshalled
      * @return
      * @throws IOException
      */
     public static String marshall(Model restModel) throws IOException {
-
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(restModel);
     }
 
     /**
-     *  Converts an json String into a object of the specific class type.
-     * @param json json string
-     * @param klazz the AbstractRestModel class to be used
+     * Converts an json String into a object of the specific class type.
+     * 
+     * @param json
+     *            json string
+     * @param klazz
+     *            the AbstractRestModel class to be used
      * @return
      * @throws IOException
      */
-    public static Model unMarshall(String json, Class<? extends Model> klazz) throws IOException {
-
+    public static <T extends Model> T unMarshall(String json, Class<T> klazz) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(json, klazz);
     }
 
-    public static List<Model> unMarshallList(String json, Class<? extends Model> klazz) throws IOException {
-
+    public static <T extends Model> List<T> unMarshallList(String json, Class<T> klazz) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, klazz));
     }
-
-
 
 }
